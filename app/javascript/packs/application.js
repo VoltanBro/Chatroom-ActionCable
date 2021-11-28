c657302de7bbbda62f24904393ed7c289854cf72
Vitalii Molchanov <32171452+VoltanBro@users.noreply.github.com>
2021-11-28 21:01:40 +0200

    Implement chatroom (#1)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,46 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import chatRoomChannel from "../channels/chat_room_channel";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+$(document).on('turbolinks:load', function () {
+
+  $("form#set_name").on('submit', function(e){
+
+    e.preventDefault();
+
+    let name = $('#add_name').val();
+
+    sessionStorage.setItem('chat_room_name', name)
+
+    chatRoomChannel.announce({ name, type: 'join'})
+
+    $("#modal").css('display', 'none');
+
+  });
+
+
+  $("form#send_message").on('submit', function(e){
+
+    e.preventDefault();
+
+    let message = $('#message').val();
+
+    if (message.length > 0) {
+
+      chatRoomChannel.speak(message);
+
+      $('#message').val('')
+
+    }
+
+  });
+
+
+  $(window).on('beforeunload', function() {
+
+    let name = sessionStorage.getItem('chat_room_name')
+
+    chatRoomChannel.announce({ name, type: 'leave'})
+
+  });
+
+})
